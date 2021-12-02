6f08591c57571c46d488b5f2777e561493aff0b7
Wayne Feltham <wayne.feltham@zoopla.co.uk>
2021-12-02 17:38:50 +0000

    tidy solution and add template project

---
+++ Template/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        static void Part1()
+        {
+
+        }
+
+        static void Part2()
+        {
+
+        }
+
+        static string[] GetData()
+        {
+            var filename = "sample-data.txt";
+#if !DEBUG
+            filename = "data.txt";
+#endif
+            return System.IO.File.ReadAllLines(filename);
+        }
+    }
+}
